dadc94b269d73efadb1cea4bf0a627746e4d5ee2
yetist <yetist@users.noreply.github.com>
2021-06-25 06:30:28 +0000

    deploy: dd3ec000f57d1c9d794c14be51de49910c666fa3

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+$('body').watermark({
+    texts : ["Preview Website", "LoongArch", "PREVIEW_COMMITID"],
+    textColor : "#d2d2d2",
+    textFont : '14px Sans',
+    width : 150,
+    height : 150,
+    textRotate : -30
+});
+});
