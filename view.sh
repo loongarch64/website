f2b5733928d4f2a4c8687cb0d67c593a42fdfec6
Xiaotian Wu <yetist@gmail.com>
2022-06-04 18:12:53 +0800

    view.sh: update for new hogu version

--- view.sh
+++ view.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
-hugo --i18n-warnings --disableFastRender --ignoreCache server
+ver=`hugo version|awk '{print $2}'`
+version=${ver#v*}
+result=`vercmp $version 0.7`
+
+if [ $result -gt 0 ]; then
+  hugo --printI18nWarnings --disableFastRender --ignoreCache server
+else
+  hugo --i18n-warnings --disableFastRender --ignoreCache server
+fi
