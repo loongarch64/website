f52b6fe77fd6b0d3d5089120e6c694a1a02ec0b8
Zhang Fuxin <fxzhang@ict.ac.cn>
2022-08-09 15:42:08 +0800

    update view.sh to improve multiple os support: 1. use /usr/bin/env to locate bash instead of /usr/bin/bash 2. write a version compare function in shell syntax to avoid the    dependence on vercmp program.

--- view.sh
+++ view.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+verlte() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
 
 ver=`hugo version|awk '{print $2}'`
 version=${ver#v*}
-result=`vercmp $version 0.7`
 
-if [ $result -gt 0 ]; then
+if verlte 0.7 $version ; then
   hugo --printI18nWarnings --disableFastRender --ignoreCache server
 else
   hugo --i18n-warnings --disableFastRender --ignoreCache server
